9148155c7f9740aca9859576f42ba5fa3e888f16
seven <2260006962@qq.com>
2023-11-14 09:55:48 +0800

    添加签到之间的间隔时间

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import random
+import time
+
 import aliyundrive_check_in
 import weibo_supertopic_chickin
 
 if __name__ == '__main__':
     weibo_supertopic_chickin.main()
+    time.sleep(random.randint(3, 7))
     aliyundrive_check_in.main()
